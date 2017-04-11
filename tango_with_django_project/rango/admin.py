65ebe9df12344553f7c79be69d7cdaf048fedec9
zhengt <tony_zheng35@hotmail.com>
2017-04-11 16:42:51 +0100

    set-up user login details, and created user profiles. Created base html template.

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from rango.models import Category, Page
 
+from rango.models import UserProfile
+
 class PageAdmin (admin.ModelAdmin):
     list_display = ( 'title', 'category', 'url',)
 
@@ -13,3 +15,4 @@ class CategoryAdmin (admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(UserProfile)
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -101,3 +101,7 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
+
+PASSWORD_HASHERS = ('django.contrib.auth.hashers.PBKDF2PasswordHasher', 'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',)
+
+LOGIN_URL = '/rango/login/'
